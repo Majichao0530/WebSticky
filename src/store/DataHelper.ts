689ebd312f9941c878e72178fff5d728bd1fa0fd
MarJaii <majichao0530@outlook.com>
2020-05-05 18:13:10 +0800

    update dist

--- src/store/DataHelper.ts
+++ src/store/DataHelper.ts
@@ -12,6 +12,8 @@ class DataHelper {
     // 1.读取全部数据，返回数组(如果没有读到数据 就返回 空数组)------------
     readData(): any {
         //1.读取 本地数据(根据dataKey读取)
+        var memo = [{"id":1,"cateid":2,"title":"Vue 3.0 更新计划","content":"更快，更小，更易维护，更易于原生，让开发者更轻松。","createTime":"2020-5-5 17:48"},{"id":2,"cateid":1,"title":"英雄联盟官宣季中赛取消","content":"2020年季中赛取消了，拳头方面开始为今年的全球总决赛开始进行准备，这或许是今年《英雄联盟》将举行的唯一赛事。由于季中赛的取消，今年S赛也做出了一些细微的调整。今年S赛的种子名额分配也发生了变化，中国及欧洲赛区将拥有四个种子队伍名额。北美和韩国仍然是三个种子队伍。","createTime":"2020-5-5 17:49"},{"id":3,"cateid":1,"title":"信用卡还款","content":"5月9日 113元\n5月20日 530元","createTime":"2020-5-5 17:52"},{"id":4,"cateid":1,"title":"五月读书挑战","content":"微博朋友圈睡前无所事事时间读书。精读1本，泛读2本。","createTime":"2020-5-5 17:56"},{"id":5,"cateid":1,"title":"说谎42句歌词 只有4句真话","content":"爱到深处，说谎最痛","createTime":"2020-5-5 17:58"}]
+
         let strData: string | null = localStorage.getItem(this.dataKey);
 
         //2.将 读取的 json数组字符串 转成数组对象
@@ -21,7 +23,10 @@ class DataHelper {
         }
 
         //3.返回 数组对象
-        return arrData;
+        if(arrData.length == 0){
+            arrData = memo          
+        }
+        return arrData
     }
 
     // 2.存入本地数据 -------------------------------------------------
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/WebSticky/" : "/",
-};
+  // 基本路径
+  publicPath: "./",
+}
